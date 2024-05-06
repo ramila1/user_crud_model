9197aca2b707785e0818c7a9559fcfa37ca8d94a
ramila1 <ramilashahi2799@gmail.com>
2024-05-07 05:30:00 +0545

    views added

--- user_model_assignment/model_app/views.py
+++ user_model_assignment/model_app/views.py
@@ -1,3 +1,34 @@
 from django.shortcuts import render
+from.models import CustomUser
+from.serializers import UserSerializer
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-# Create your views here.
+class CreateUser(APIView):
+    def post(self, request):
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors)
+
+class ReadUser(APIView):
+    def get(self, request):
+        users = CustomUser.objects.all()
+        serializer = UserSerializer(users, many=True)
+        return Response(serializer.data)
+
+class UpdateUser(APIView):
+    def put(self, request, pk):
+        user = CustomUser.objects.get(pk=pk)
+        serializer = UserSerializer(user, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors)
+
+class DeleteUser(APIView):
+    def delete(self, request, pk):
+        user = CustomUser.objects.get(pk=pk)
+        user.delete()
+        return Response({'message': 'User deleted successfully'})
