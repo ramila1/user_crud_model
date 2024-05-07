cc49e71f810f7fddc5c4a129e56682d2e310ee6e
ramila1 <ramilashahi2799@gmail.com>
2024-05-07 08:02:12 +0545

    edit managers

--- user_model_assignment/model_app/managers.py
+++ user_model_assignment/model_app/managers.py
@@ -1,14 +1,11 @@
 
 from django.contrib.auth.base_user import BaseUserManager
-from django.utils.translation import gettext_lazy as _
-
-
 class CustomUserManager(BaseUserManager):
 
     def create_user(self, email, password, **extra_fields):
  
         if not email:
-            raise ValueError(_("The Email must be set"))
+            raise ValueError(("The Email must be set"))
         email = self.normalize_email(email)
         user = self.model(email=email, **extra_fields)
         user.set_password(password)
@@ -21,9 +18,9 @@ class CustomUserManager(BaseUserManager):
         extra_fields.setdefault("is_superuser", True)
         extra_fields.setdefault("is_active", True)
         if extra_fields.get("is_staff") is not True:
-            raise ValueError(_("Superuser must have is_staff=True."))
+            raise ValueError(("Superuser must have is_staff=True."))
         if extra_fields.get("is_superuser") is not True:
-            raise ValueError(_("Superuser must have is_superuser=True."))
+            raise ValueError(("Superuser must have is_superuser=True."))
         return self.create_user(email, password, **extra_fields)
 
 
