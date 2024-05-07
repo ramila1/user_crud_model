6ee2bde7a1df44ea6002af332f4131b5c9d7ab0e
ramila1 <ramilashahi2799@gmail.com>
2024-05-07 07:10:35 +0545

    edit app urls pattern

--- user_model_assignment/model_app/urls.py
+++ user_model_assignment/model_app/urls.py
@@ -19,7 +19,6 @@
 # users/urls.py
 from django.urls import path
 from .views import CustomUserListView, CustomUserDetailView
-
 urlpatterns = [
     path('', CustomUserListView.as_view()),
     path('<int:pk>/', CustomUserDetailView.as_view()),
