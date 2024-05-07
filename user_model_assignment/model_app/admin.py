d89d78ef39d5b31f4ee07c2ad1a83286b37ad673
ramila1 <ramilashahi2799@gmail.com>
2024-05-07 07:12:30 +0545

    register customuser model

--- user_model_assignment/model_app/admin.py
+++ user_model_assignment/model_app/admin.py
@@ -2,6 +2,7 @@ from .models import CustomUser
 from django.contrib import admin
 from django.contrib.auth.models import User
 from django.contrib.auth.admin import UserAdmin
+
 # Register your models here.
 admin.site.register(CustomUser)
 
