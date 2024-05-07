6605fbabecf197f79b386d38bf863443e72a762b
ramila1 <ramilashahi2799@gmail.com>
2024-05-07 07:21:19 +0545

    edit urls of project

--- user_model_assignment/user_model_assignment/urls.py
+++ user_model_assignment/user_model_assignment/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 from django.urls import path,include
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('model_app.urls')),
+    path('api/users', include('model_app.urls')),
 ]
 
