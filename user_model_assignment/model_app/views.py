703d7cc0c1a190137af17575b5e53c9d966291c2
ramila1 <ramilashahi2799@gmail.com>
2024-05-07 07:37:29 +0545

    added patch, options

--- user_model_assignment/model_app/views.py
+++ user_model_assignment/model_app/views.py
@@ -33,13 +33,14 @@
 #         user.delete()
 #         return Response({'message': 'User deleted successfully'})
 
-from django.shortcuts import render
-# Create your views here.
-# users/views.py
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from.models import CustomUser
-from.serializers import UserSerializer
+from rest_framework import status
+from rest_framework.parsers import JSONParser
+from django.http import HttpResponse
+from django.shortcuts import render
+from .models import CustomUser
+from .serializers import UserSerializer
 
 class CustomUserListView(APIView):
     def get(self, request):
@@ -51,15 +52,15 @@ class CustomUserListView(APIView):
         serializer = UserSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data, status=201)
-        return Response(serializer.errors, status=400)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 class CustomUserDetailView(APIView):
     def get_object(self, pk):
         try:
             return CustomUser.objects.get(pk=pk)
         except CustomUser.DoesNotExist:
-            return Response(status=404)
+            return Response(status=status.HTTP_404_NOT_FOUND)
 
     def get(self, request, pk):
         user = self.get_object(pk)
@@ -72,9 +73,23 @@ class CustomUserDetailView(APIView):
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
-        return Response(serializer.errors, status=400)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def patch(self, request, pk):
+        user = self.get_object(pk)
+        serializer = UserSerializer(user, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     def delete(self, request, pk):
         user = self.get_object(pk)
         user.delete()
-        return Response(status=204)
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+    def options(self, request, *args, **kwargs):
+        response = HttpResponse()
+        response['Allow'] = 'GET, POST, PUT, PATCH, DELETE, OPTIONS'
+        response['Content-Type'] = 'application/json'
+        return response
