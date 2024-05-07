0d61da9df6d2d02d43213de864e8005bb4f7dd93
ramila1 <ramilashahi2799@gmail.com>
2024-05-07 07:53:16 +0545

    edit views

--- user_model_assignment/model_app/views.py
+++ user_model_assignment/model_app/views.py
@@ -1,37 +1,3 @@
-# from django.shortcuts import render
-# from.models import CustomUser
-# from.serializers import UserSerializer
-# from rest_framework.response import Response
-# from rest_framework.views import APIView
-
-# class CreateUser(APIView):
-#     def post(self, request):
-#         serializer = UserSerializer(data=request.data)
-#         if serializer.is_valid():
-#             serializer.save()
-#             return Response(serializer.data)
-#         return Response(serializer.errors)
-
-# class ReadUser(APIView):
-#     def get(self, request):
-#         users = CustomUser.objects.all()
-#         serializer = UserSerializer(users, many=True)
-#         return Response(serializer.data)
-
-# class UpdateUser(APIView):
-#     def put(self, request, pk):
-#         user = CustomUser.objects.get(pk=pk)
-#         serializer = UserSerializer(user, data=request.data)
-#         if serializer.is_valid():
-#             serializer.save()
-#             return Response(serializer.data)
-#         return Response(serializer.errors)
-
-# class DeleteUser(APIView):
-#     def delete(self, request, pk):
-#         user = CustomUser.objects.get(pk=pk)
-#         user.delete()
-#         return Response({'message': 'User deleted successfully'})
 
 from rest_framework.response import Response
 from rest_framework.views import APIView
@@ -93,3 +59,5 @@ class CustomUserDetailView(APIView):
         response['Allow'] = 'GET, POST, PUT, PATCH, DELETE, OPTIONS'
         response['Content-Type'] = 'application/json'
         return response
+
+
