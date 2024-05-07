9a66d5a75fc69e6ce07bbe8523d379ceff0bf5b7
ramila1 <ramilashahi2799@gmail.com>
2024-05-07 07:59:40 +0545

    edit models

--- user_model_assignment/model_app/models.py
+++ user_model_assignment/model_app/models.py
@@ -1,11 +1,11 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
-from django.utils.translation import gettext_lazy as _
+
 
 from .managers import CustomUserManager
 
 class CustomUser(AbstractBaseUser, PermissionsMixin):
-    email = models.EmailField(_("email address"), unique=True)
+    email = models.EmailField(("email address"), unique=True)
     phone = models.IntegerField(unique=True, null=True)
     address = models.CharField(max_length=50, null=True)
     gender = models.CharField(max_length=10, choices=(('male', 'Male'), ('female', 'Female'), ('other', 'Other')))
